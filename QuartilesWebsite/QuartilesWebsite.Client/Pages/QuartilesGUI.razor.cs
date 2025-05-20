75a4263efedcf9b9e20fd332e7350760cc290d3d
Alex Lancaster <u1505710@umail.utah.edu>
2025-05-20 01:16:56 -0500

    Added RemoveDuplicates Method

--- QuartilesWebsite/QuartilesWebsite.Client/Pages/QuartilesGUI.razor.cs
+++ QuartilesWebsite/QuartilesWebsite.Client/Pages/QuartilesGUI.razor.cs
@@ -7,35 +7,38 @@ namespace QuartilesWebsite.Client.Pages
 {
     public partial class QuartilesGUI : ComponentBase
     {
-        public int MAX_CHUNK_LENGTH;
-        public int rows;
-        public int columns;
+        public int MaxChunkLength { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
 
-        public List<string> GridValues;
-        public QuartilesCracker solver;
-        public List<Chunk> chunkList;
+        private QuartilesCracker solver = new();
+        protected List<Chunk> chunkList = new();
 
         public List<string> Results { get; set; } = new();
+        public List<KeyValuePair<string, List<string>>> SolutionChunkMapping { get; set; } = new();
 
         [Inject]
         private IJSRuntime JS { get; set; } = default!;
 
-        public QuartilesGUI()
+        public QuartilesGUI(int maxChunkLength = 5, int rows = 5, int columns = 4)
         {
-            MAX_CHUNK_LENGTH = 5;
-            rows = 5;
-            columns = 4;
+            MaxChunkLength = maxChunkLength;
+            Rows = rows;
+            Columns = columns;
+        }
 
-            GridValues = new();
-            solver = new();
-            chunkList = new();
+        public QuartilesGUI() 
+        {
+            MaxChunkLength = 5;
+            Rows = 5;
+            Columns = 4;
         }
 
         protected override void OnInitialized()
         {
-            for (int row = 0; row < rows; row++)
+            for (int row = 0; row < Rows; row++)
             {
-                for (int col = 0; col < columns; col++)
+                for (int col = 0; col < Columns; col++)
                 {
                     Chunk cell = new Chunk(row, col);
                     chunkList.Add(cell);
@@ -43,25 +46,32 @@ namespace QuartilesWebsite.Client.Pages
             }
         }
 
-        public Chunk GetChunk(int row, int column)
+        protected Chunk GetChunk(int row, int column)
         {
             return chunkList.FirstOrDefault(c => c.Row == row && c.Column == column);
         }
 
-        public async Task SubmitGrid()
+        protected async Task SubmitGrid()
         {
             if (chunkList.Any(c => string.IsNullOrWhiteSpace(c.Letters)))
             {
                 await JS.InvokeVoidAsync("alert", "Please fill in all available cells in the table.");
             }
 
+            DisplaySolutions();
+        }
+
+        protected async Task DisplaySolutions()
+        {
             List<string> chunkLetters = new();
             foreach (Chunk cell in chunkList)
             {
                 chunkLetters.Add(cell.Letters);
             }
 
-            Results = solver.QuartilesDriver(chunkLetters);
+            var (results, solutionChunkMapping) = solver.QuartileSolver(chunkLetters);
+            this.Results = results;
+            this.SolutionChunkMapping = solutionChunkMapping;
         }
     }
 }

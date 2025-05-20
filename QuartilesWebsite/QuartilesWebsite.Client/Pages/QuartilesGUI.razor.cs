0e3a931f175d1557a3460a3f02bd4019aa1def1d
Alex Lancaster <u1505710@umail.utah.edu>
2025-05-19 22:51:56 -0500

    Refactor Chunk and QuartilesCracker functionality

--- QuartilesWebsite/QuartilesWebsite.Client/Pages/QuartilesGUI.razor.cs
+++ QuartilesWebsite/QuartilesWebsite.Client/Pages/QuartilesGUI.razor.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using Quartiles;
+using Chunks;
 
 namespace QuartilesWebsite.Client.Pages
 {
@@ -11,6 +13,13 @@ namespace QuartilesWebsite.Client.Pages
 
         public List<string> GridValues;
         public QuartilesCracker solver;
+        public List<Chunk> chunkList;
+
+        public List<string> Results { get; set; } = new();
+
+        [Inject]
+        private IJSRuntime JS { get; set; } = default!;
+
         public QuartilesGUI()
         {
             MAX_CHUNK_LENGTH = 5;
@@ -19,27 +28,40 @@ namespace QuartilesWebsite.Client.Pages
 
             GridValues = new();
             solver = new();
+            chunkList = new();
         }
-
 
         protected override void OnInitialized()
         {
             for (int row = 0; row < rows; row++)
             {
-
                 for (int col = 0; col < columns; col++)
                 {
-                    GridValues.Add(string.Empty);
+                    Chunk cell = new Chunk(row, col);
+                    chunkList.Add(cell);
                 }
             }
         }
 
-        private async Task SubmitGrid()
+        public Chunk GetChunk(int row, int column)
         {
-            if (GridValues.Contains(string.Empty))
-            {
+            return chunkList.FirstOrDefault(c => c.Row == row && c.Column == column);
+        }
 
+        public async Task SubmitGrid()
+        {
+            if (chunkList.Any(c => string.IsNullOrWhiteSpace(c.Letters)))
+            {
+                await JS.InvokeVoidAsync("alert", "Please fill in all available cells in the table.");
             }
+
+            List<string> chunkLetters = new();
+            foreach (Chunk cell in chunkList)
+            {
+                chunkLetters.Add(cell.Letters);
+            }
+
+            Results = solver.QuartilesDriver(chunkLetters);
         }
     }
 }
